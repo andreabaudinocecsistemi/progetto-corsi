7021d66e07d15ecf65d3b2aa95fb5af0529f09bb
ITS2_03 <ITS2_03@10.0.2.62>
2025-09-24 10:51:01 +0200

    fix java project

---
+++ src/it/ruffini/progettoCorsi/Main.java
@@ -0,0 +1,17 @@
+package it.ruffini.progettoCorsi;
+
+public class Main {
+
+	public Main() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		FrameBase frame = new FrameBase();
+		frame.start();
+		
+		frame.setVisible(true);
+	}
+}
+
