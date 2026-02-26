46ce06e0ddfb299974804334613385470e00a5ca
Andrea Baudino <andrea.baudino@cecsistemi.it>
2026-02-26 08:48:49 +0100

    Commit di prova per issue di prova
    Added comment for frame initialization in main method.

--- Main.java
+++ Main.java
@@ -7,11 +7,13 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		
+
+		//inizializzazione del frame di base
 		FrameBase frame = new FrameBase();
 		frame.start();
 		
 		frame.setVisible(true);
 	}
 }
+
 
